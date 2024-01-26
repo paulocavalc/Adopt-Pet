80a3c0768bff6b1a8607e6c8a4eb5094cd45c691
João Paulo <paulosantiago.cavalcante@gmail.com>
2024-01-25 21:58:19 -0300

    Renomear

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, library_private_types_in_public_api
 
-import 'package:boa_tarde/views/home.dart';
+import 'package:boa_tarde/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
