f31c5d4e00000fef31750dfd6e96bd98942c4361
João Paulo <paulosantiago.cavalcante@gmail.com>
2024-01-27 17:11:40 -0300

    Main Hoje

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,29 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors_in_immutables, unused_field, unused_element
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors_in_immutables, unused_field, unused_element, prefer_const_literals_to_create_immutables
 
 import 'package:boa_tarde/authentication/login.dart';
-import 'package:boa_tarde/loading/splash.dart';
-import 'package:boa_tarde/model/wallpaper.dart';
-import 'package:boa_tarde/stories/story.dart';
+import 'package:boa_tarde/wallpapers/cat.dart';
+import 'package:boa_tarde/wallpapers/dog.dart';
+import 'package:boa_tarde/loading/loading.dart';
+import 'package:boa_tarde/wallpapers/dove.dart';
+import 'package:boa_tarde/wallpapers/duck.dart';
+import 'package:boa_tarde/wallpapers/fish.dart';
+import 'package:boa_tarde/wallpapers/giraffe.dart';
+import 'package:boa_tarde/wallpapers/hen.dart';
+import 'package:boa_tarde/wallpapers/horse.dart';
+import 'package:boa_tarde/wallpapers/penguin.dart';
+import 'package:boa_tarde/wallpapers/squirrel.dart';
+import 'package:boa_tarde/wallpapers/turtle.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Adopt Pet',
-      home: SplashScreen(),
+      home: LoadingScreen(),
     );
   }
 }
@@ -28,29 +38,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List _images = [
-    Wallpaper(image: "assets/images/pet0.jpg"),
-    Wallpaper(image: "assets/images/pet1.jpg"),
-    Wallpaper(image: "assets/images/pet2.jpg"),
-    Wallpaper(image: "assets/images/pet3.jpg"),
-    Wallpaper(image: "assets/images/pet4.jpg"),
-    Wallpaper(image: "assets/images/pet5.jpg"),
-    Wallpaper(image: "assets/images/pet6.jpg"),
-    Wallpaper(image: "assets/images/pet7.jpg"),
-    Wallpaper(image: "assets/images/pet8.jpg"),
-    Wallpaper(image: "assets/images/pet9.jpg"),
-    Wallpaper(image: "assets/images/pet10.jpg"),
-    Wallpaper(image: "assets/images/pet11.jpg"),
-    Wallpaper(image: "assets/images/pet12.jpg"),
-    Wallpaper(image: "assets/images/pet13.jpg"),
-    Wallpaper(image: "assets/images/pet14.jpg"),
-    Wallpaper(image: "assets/images/pet15.jpg"),
-    Wallpaper(image: "assets/images/pet16.jpg"),
-    Wallpaper(image: "assets/images/pet17.jpg"),
-    Wallpaper(image: "assets/images/pet18.jpg"),
-    Wallpaper(image: "assets/images/pet19.jpg"),
-  ];
-
+  
   int _index = 0;
 
   void _onItemTapped(int index) {
@@ -81,65 +69,101 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(0, 0, 0, 1),
-        centerTitle: true,
-        title: Center(
-          child: Text("Adopt Pet",
-              style: GoogleFonts.calistoga(
-                  color: Color.fromARGB(255, 255, 98, 0), fontSize: 33)),
+    return DefaultTabController(
+      length: 20,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+          centerTitle: true,
+          title: Center(
+            child: Text("Adopt Pet",
+                style: GoogleFonts.calistoga(
+                    color: Color.fromARGB(255, 255, 98, 0), fontSize: 33)),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(
+                text: 'Cachorro',
+              ),
+              Tab(
+                text: 'Cavalo',
+              ),
+              Tab(
+                text: 'Esquilo',
+              ),
+              Tab(
+                text: 'Galinha',
+              ),
+              Tab(
+                text: 'Gato',
+              ),
+              Tab(
+                text: 'Girafa',
+              ),
+              Tab(
+                text: 'Pato',
+              ),
+              Tab(
+                text: 'Peixe',
+              ),
+              Tab(
+                text: 'Pinguim',
+              ),
+              Tab(
+                text: 'Pomba',
+              ),
+              Tab(
+                text: 'Tartaruga',
+              ),
+            ],
+          ),
         ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(50),
-          child: StoryBar(),
+        body: TabBarView(
+          children: [
+            Tela_Cachorro(),
+            Tela_Cavalo(),
+            Tela_Esquilo(),
+            Tela_Galinha(),
+            Tela_Gato(),
+            Tela_Girafa(),
+            Tela_Pato(),
+            Tela_Peixe(),
+            Tela_Pinguim(),
+            Tela_Pomba(),
+            Tela_Tartaruga(),
+          ],
         ),
-      ),
-      backgroundColor: Color.fromARGB(255, 0, 0, 0),
-      body: GridView.builder(
-        itemCount: _images.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1,
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Início',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Pesquisa',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle),
+              label: 'Adicionar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoritos',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Perfil',
+            ),
+          ],
+          currentIndex: _index,
+          onTap: _onItemTapped,
+          selectedItemColor: Color.fromARGB(255, 255, 98, 0),
+          unselectedItemColor: Color.fromARGB(255, 151, 151, 151),
         ),
-        itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            _images[index].image,
-            fit: BoxFit.cover,
-          );
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Início',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Pesquisa',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            label: 'Adicionar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favoritos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Minha Conta',
-          ),
-        ],
-        currentIndex: _index,
-        onTap: _onItemTapped,
-        selectedItemColor: Color.fromARGB(255, 255, 98, 0),
-        unselectedItemColor: Color.fromARGB(255, 151, 151, 151),
       ),
     );
   }
---
+++ lib/loading/loading.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unnecessary_new, sized_box_for_whitespace, sort_child_properties_last
+import 'dart:async';
+import 'package:boa_tarde/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoadingScreen());
+}
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = Duration(seconds: 4);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => MyHomePage(title: 'Adopt Pet')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(255, 0, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                child: Image.asset("assets/icons/logo.png"),
+                width: 150,
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
